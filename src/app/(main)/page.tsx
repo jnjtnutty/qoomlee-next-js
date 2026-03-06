c5c5085b03b07eed5723a7c59cb4bc986ad32716
pattaraporn punya <pattaraporn.p@arise.tech>
2026-03-06 11:39:42 +0700

    refactor: remove unused MediaQueryContext import from Home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useMediaQueryContext } from '@/context/MediaQueryContext';
-
 export default function Home() {
 
   return <div className='w-full h-screen flex justify-center items-center'>
